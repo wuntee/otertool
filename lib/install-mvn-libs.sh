3df2ff7fe7aa7e4ec84cde757e9bed7e286f0c9c
Mathew Rowley <mathew.rowley@gmail.com>
2012-04-27 09:18:02 -0500

    - thotcon!!

--- lib/install-mvn-libs.sh
+++ lib/install-mvn-libs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mvn install:install-file -Dfile=sdklib/sdklib-r16.jar -DgroupId=com.android.sdklib -DartifactId=sdklib -Dversion=r16 -Dpackaging=jar
+mvn install:install-file -Dfile=ddms/ddms-r17.jar -DgroupId=com.android.ddms -DartifactId=ddms -Dversion=r17 -Dpackaging=jar
+mvn install:install-file -Dfile=ddms/ddmlib-r17.jar -DgroupId=com.android.ddmlib -DartifactId=ddmlib -Dversion=r17 -Dpackaging=jar
 mvn install:install-file -Dfile=smali/smali-1.2.6.jar -DgroupId=antlr -DartifactId=smali -Dversion=1.2.6 -Dpackaging=jar
 mvn install:install-file -Dfile=smali/baksmali-1.2.6.jar -DgroupId=antlr -DartifactId=baksmali -Dversion=1.2.6 -Dpackaging=jar
 mvn install:install-file -Dfile=swt-3.7-cocoa-macosx/swt.jar -DgroupId=org.eclipse.swt -DartifactId=swt -Dversion=3.7-cocoa-macosx -Dpackaging=jar
